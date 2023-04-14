7387ec830c2e026c988e19c6b89c666936d028d6
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-14 15:52:29 +0200

    Aggiungere i file di progetto.

---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            try
+            {
+                List<Indirizzo> indirizzi = new List<Indirizzo>();
+
+                StreamReader file = File.OpenText("addresses.csv");
+                
+                while (!file.EndOfStream)
+                {
+                    if (i > 0)
+                    {
+                        string riga = file.ReadLine();
+                        string[] parole = riga.Split(",");
+
+                        string name = parole[0];
+                        string surname = parole[1];
+                        string street = parole[2];
+                        string city = parole[3];
+                        string province = parole[4];
+                        string zip = parole[5];
+
+                        Indirizzo newIndirizzo = new Indirizzo(name, surname, street, city, province, zip);
+                        Console.WriteLine($"Indirizzo n° {i}: {name}, {surname}, {street}, {city}, {province}, {zip}\n");
+                        indirizzi.Add(newIndirizzo);
+                    }
+                    i++;
+                    
+                }
+
+                file.Close();
+            }
+            catch(Exception error)
+            {
+                Console.WriteLine($"Si è verificato un errore con l'indirizzo n: '{i}' {error.Message}\n");
+            }
+            
+
+
+        }
+    }
+}
