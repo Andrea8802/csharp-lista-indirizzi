9601a542c1cf0130b414f9c7992e223c4126165e
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-04-15 14:49:04 +0200

    Fixato gestione error

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,45 +1,65 @@
-﻿namespace csharp_lista_indirizzi
+﻿using System.IO;
+using System.Net;
+using System.Xml.Linq;
+
+namespace csharp_lista_indirizzi
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Indirizzo> addresses = new List<Indirizzo>();
+
+            Console.WriteLine("------------------\n");
+
+            StreamReader file = File.OpenText("addresses.csv");
+
             int i = 0;
-            try
+            while (!file.EndOfStream)
             {
-                List<Indirizzo> indirizzi = new List<Indirizzo>();
 
-                StreamReader file = File.OpenText("addresses.csv");
-                
-                while (!file.EndOfStream)
+                try
                 {
-                    if (i > 0)
+                    string? line = file.ReadLine();
+                    string[] words = line.Split(',');
+
+                    if (i < 1)
                     {
-                        string riga = file.ReadLine();
-                        string[] parole = riga.Split(",");
-
-                        string name = parole[0];
-                        string surname = parole[1];
-                        string street = parole[2];
-                        string city = parole[3];
-                        string province = parole[4];
-                        string zip = parole[5];
-
-                        Indirizzo newIndirizzo = new Indirizzo(name, surname, street, city, province, zip);
-                        Console.WriteLine($"Indirizzo n° {i}: {name}, {surname}, {street}, {city}, {province}, {zip}\n");
-                        indirizzi.Add(newIndirizzo);
+                        i++;
+                        continue;
                     }
-                    i++;
-                    
+
+                    if (words.Length == 6)
+                    {
+                        string name = words[0];
+                        string surname = words[1];
+                        string street = words[2];
+                        string city = words[3];
+                        string province = words[4];
+                        string zip = words[5];
+
+                        Console.WriteLine($"Indirizzo n° {i}: {name}, {surname}, {street}, {city}, {province}, {zip}\n");
+
+                        addresses.Add(new Indirizzo(name, surname, street, city, province, zip));
+                    }
+                    else
+                    {
+                        throw new Exception($"L'indirizzo n° {i} contiene uno o più campi vuoti!");
+                    }
+
+                    Console.WriteLine("------------------\n");
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Errore! " + e.Message);
+                    Console.WriteLine("\n------------------\n");
                 }
 
-                file.Close();
+                i++;
             }
-            catch(Exception error)
-            {
-                Console.WriteLine($"Si è verificato un errore con l'indirizzo n: '{i}' {error.Message}\n");
-            }
-            
+
+            file.Close();
 
 
         }
